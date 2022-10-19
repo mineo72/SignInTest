b22a1537b739346087cdb8d4f8a8a68681fb574a
Micah Olson <mineo72inmc@gmail.com>
2022-10-19 14:12:48 -0400

    a gallon of psp

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -56,6 +56,7 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(12, 43);
             this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(193, 23);
             this.textBox2.TabIndex = 2;
             // 
@@ -83,13 +84,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(217, 204);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
--- Form1.cs
+++ Form1.cs
@@ -13,36 +13,56 @@ namespace WinFormsApp16
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SoundPlayer soundPlayer = new SoundPlayer();
+            SoundPlayer soundPlayer = new();
             soundPlayer.SoundLocation = @"C:\Users\23OlsonM\Downloads\[ONTIVA.COM] cringe, there's no other word for it-HQ.wav";
             soundPlayer.Play();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            
-
+            SignIn();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             string connStr = "server=localhost;user=root;database=dotnet;port=3306;password=Got22259";
-            MySqlConnection conn = new MySqlConnection(connStr);
+            MySqlConnection conn = new(connStr);
             conn.Open();
             string takenCheck = $"SELECT userName FROM user WHERE userName = '{textBox1.Text}'";
-            MySqlCommand cmd = new MySqlCommand(takenCheck, conn);
+            MySqlCommand cmd = new(takenCheck, conn);
             MySqlDataReader rdr = cmd.ExecuteReader();
-            rdr.Read();
-            if (rdr[0].ToString() == textBox1.Text)
+            if (rdr.HasRows)
             {
                 MessageBox.Show("Taken");
+                conn.Close();
             }
             else
             {
-                MySqlCommand insComm = new($"INSERT INTO user (userName, userPassword) VALUES ({textBox1.Text}, {textBox2.Text})");
+                rdr.Close();
+                MySqlCommand insComm = new($"INSERT INTO user(userName, userPass) VALUES('{textBox1.Text}', '{textBox2.Text}')",conn);
                 insComm.ExecuteNonQuery();
                 MessageBox.Show("Signed Up!");
+                conn.Close();
             }
+        }
+
+        void SignIn()
+        {
+            string connStr = "server=localhost;user=root;database=dotnet;port=3306;password=Got22259";
+            MySqlConnection conn = new(connStr);
+            conn.Open();
+            string takenCheck = $"SELECT userName FROM user WHERE userName = '{textBox1.Text}' AND userPass ='{textBox2.Text}'";
+            MySqlCommand cmd = new(takenCheck, conn);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            if (rdr.HasRows)
+            {
+                MessageBox.Show("Logged in");
+            }
+            else
+            {
+                MessageBox.Show("Invalid Login");
+            }
+            conn.Close();
         }
     }
 }
